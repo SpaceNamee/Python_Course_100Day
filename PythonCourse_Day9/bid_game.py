3726ba6d49017ccfa3eebc526223e5d3ff1c10ba
Marianna <mariannabenkalovych@gmail.com>
2025-01-27 10:45:04 +0200

    Completed task by myself

---
+++ PythonCourse_Day9/bid_game.py
@@ -0,0 +1,37 @@
+import re
+
+print(r'''
+ ____  _     _  
+| __ )(_) __| | 
+|  _ \| |/ _` | 
+| |_) | | (_| | 
+|____/|_|\__,_| 
+''')
+bidder = {}
+turn = True
+
+while turn:
+    name = input("Enter your name: ")
+    bid = input("Enter your bid in $: ")
+    if re.search("[a-z]", bid) != None :
+        print("Invalid input")
+        continue
+    bidder[name] = int(bid)
+    while True:
+        another_bidder = input("There is another bidder? (yes, no): ")
+        if another_bidder == "no":
+            max = 0
+            winner = 'noone'
+            for key in bidder:
+                if bidder[key] > max:
+                    max = bidder[key]
+                    winner = key
+                elif bidder[key] == max:
+                    winner += " " + key
+            turn = False
+            print(f"Winner: {winner} bid: {max}")
+            break
+        elif another_bidder == "yes":
+            break
+        else:
+            print("Invalid input.")
