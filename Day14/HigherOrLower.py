329d386bcb3850ce89464fe9dd2e2c3c8bd33873
Marianna <mariannabenkalovych@gmail.com>
2025-02-03 23:25:00 +0200

    added function

--- Day14/HigherOrLower.py
+++ Day14/HigherOrLower.py
@@ -4,6 +4,34 @@ import ascii_art
 from game_data import data
 import random
 
+def print_comparable_statement(i):
+    """print a comparable statement in different way that is up to starting game or not"""
+    print(
+        "_______________________________________________________________________________________________________________")
+    if score:
+        obj_a = used_num[i]
+        print(
+            f"\nCompare A: {data[obj_a]["name"]}, {data[obj_a]["description"]}, from {data[obj_a]["country"]}, with {data[obj_a]["follower_count"]}M followers")
+    else:
+        obj_a = random.randint(0, len(data))
+        print(
+            f"\nCompare A: {data[obj_a]["name"]}, {data[obj_a]["description"]}, from {data[obj_a]["country"]}, with {data[obj_a]["follower_count"]}M followers")
+
+    print(ascii_art.vs)
+
+    while True:
+        obj_b = random.randint(0, len(data))
+        if obj_b != obj_a:
+            break
+
+    print(
+        f"Against B: {data[obj_b]["name"]}, {data[obj_b]["description"]}, from {data[obj_b]["country"]}, with {data[obj_b]["follower_count"]}M followers")
+
+    print(
+        "_______________________________________________________________________________________________________________")
+
+    return [obj_a, obj_b]
+
 print(ascii_art.logo)
 score = 0
 start = True
@@ -11,28 +39,15 @@ start = True
 used_num = [-1]
 while start:
     for i in range(0, len(data)):
-        # print a comparable statement in different way that is up to starting game or not
-        print("_______________________________________________________________________________________________________________")
-        if score:
-            obj_a = used_num[i]
-            print(f"\nCompare A: {data[obj_a]["name"]}, {data[obj_a]["description"]}, from {data[obj_a]["country"]}, with {data[obj_a]["follower_count"]}M followers")
-        else:
-            obj_a = random.randint(0, len(data))
-            print(f"\nCompare A: {data[obj_a]["name"]}, {data[obj_a]["description"]}, from {data[obj_a]["country"]}, with {data[obj_a]["follower_count"]}M followers")
 
-        print(ascii_art.vs)
+        # get objects = [obj_a, obj_b]
+        objects = print_comparable_statement(i)
+        obj_a = objects[0]
+        obj_b = objects[1]
 
-        while True:
-            obj_b = random.randint(0, len(data))
-            if obj_b != obj_a:
-                break
-
-        print(f"Against B: {data[obj_b]["name"]}, {data[obj_b]["description"]}, from {data[obj_b]["country"]}, with {data[obj_b]["follower_count"]}M followers")
-
-        print("_______________________________________________________________________________________________________________")
         # Ask the player's answer
         while True:
-            user_input = input("Who has more followers in millions (A or B)? ")
+            user_input = input("Who has more followers in millions (A or B)? ").upper()
 
             if user_input == 'A' or  user_input == 'B':
                 break
