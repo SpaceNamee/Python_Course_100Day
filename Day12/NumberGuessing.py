28e091dcd4c82ba0aeaa85fd22e631d82d776d9b
Marianna <mariannabenkalovych@gmail.com>
2025-02-03 10:03:27 +0200

    Completed task

--- Day12/NumberGuessing.py
+++ Day12/NumberGuessing.py
@@ -1,3 +1,28 @@
 from random import randint
 
-print()
+import assci_art
+
+print(assci_art.welcoming)
+print("Guess the number. Number is between 1 to 1000")
+start = 1
+end = 1000
+num = randint(start, end+1)
+print(num)
+while True:
+    user_input = int(input("Enter number: "))
+    diff = num - user_input
+    if diff > 0:
+        if diff > end / 2:
+            print("Number is too small")
+        else:
+            print("Number is small")
+    elif diff < 0:
+        if -diff > end / 2:
+            print("Number is too big")
+        else:
+            print("Number is big")
+    else:
+        print("You guess!")
+        break
+
+
