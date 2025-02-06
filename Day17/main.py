04d182d7871d3bc896036c565a792d6d7b070e1c
Marianna <mariannabenkalovych@gmail.com>
2025-02-06 14:51:03 +0200

    Added new methods

--- Day17/main.py
+++ Day17/main.py
@@ -1,4 +1,9 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = [Question(question_data[i]['text'], question_data[i]["answer"]) for i in range(len(question_data))]
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
