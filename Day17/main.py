2a2cc59ac5e181842847cb9d9acb2e1d3235a42d
Marianna <mariannabenkalovych@gmail.com>
2025-02-06 14:58:38 +0200

    Cmpleted task

--- Day17/main.py
+++ Day17/main.py
@@ -7,3 +7,6 @@ question_bank = [Question(question_data[i]['text'], question_data[i]["answer"]) 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
