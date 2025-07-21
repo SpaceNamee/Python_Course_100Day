adcf1fbfd2685a52f203663bdd5958c123ea5cc1
Marianna <mariannabenkalovych@gmail.com>
2025-07-21 03:18:27 +0300

    Register a new user and add them your database

--- Day68/main.py
+++ Day68/main.py
@@ -4,18 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
 # CREATE DATABASE
-
+file_path = os.path.abspath(__file__)
+dir_path = os.path.dirname(file_path)
+db_path = os.path.join(dir_path, "instance")
 
 class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}/users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -38,8 +41,23 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        pass_ = request.form.get("password")
+
+        new_user = User(
+            name = name,
+            email = email,
+            password = pass_
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template("secrets.html", name=name)
     return render_template("register.html")
 
 
