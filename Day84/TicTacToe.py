0e42b649bda6bbf3622b2749a4513ef0a8655fc5
Marianna <mariannabenkalovych@gmail.com>
2025-08-25 14:55:01 +0300

    Add AI player by using minimax algorithm

--- Day84/TicTacToe.py
+++ Day84/TicTacToe.py
@@ -22,27 +22,114 @@ class TicTacToe:
 
         print(LINE_H)
 
-    def check_winner(self, player):
-        if ((self.grid[0][0] == self.grid[0][1] == self.grid[0][2]) and self.grid[0][0] != BOX or
-            (self.grid[1][0] == self.grid[1][1] == self.grid[1][2]) and self.grid[1][0] != BOX or
-            (self.grid[2][0] == self.grid[2][1] == self.grid[2][2]) and self.grid[2][0] != BOX or
-            (self.grid[0][0] == self.grid[1][0] == self.grid[2][0]) and self.grid[0][0] != BOX or
-            (self.grid[0][1] == self.grid[1][1] == self.grid[2][1]) and self.grid[0][1] != BOX or
-            (self.grid[0][2] == self.grid[1][2] == self.grid[2][2]) and self.grid[0][2] != BOX or
-            (self.grid[0][0] == self.grid[1][1] == self.grid[2][2]) and self.grid[0][0] != BOX or
-            (self.grid[0][2] == self.grid[1][1] == self.grid[2][0]) and self.grid[0][2] != BOX
-            ):
-            print(f"{player.name} is the winner!")
-            player.add_score()
-            print(f"Score: {player.name}: {player.score}")
-            return True
-        return False
+    def equals3(self, a, b, c):
+        return a == b and b == c and a != BOX
+
+    def check_winner(self):
+        winner = None
+
+        # Check horizontal
+        for i in range(3):
+            if self.equals3(self.grid[i][0], self.grid[i][1], self.grid[i][2]):
+                winner = self.grid[i][0]
+
+        # Check Vertical
+        for i in range(3):
+            if (self.equals3(self.grid[0][i], self.grid[1][i], self.grid[2][i])):
+                winner = self.grid[0][i]
+
+        # Check diagonals
+        if (self.equals3(self.grid[0][0], self.grid[1][1], self.grid[2][2])):
+            winner = self.grid[0][0]
+
+        if (self.equals3(self.grid[2][0], self.grid[1][1], self.grid[0][2])):
+            winner = self.grid[2][0]
+
+        openSpots = 0;
+        for i in range(3):
+          for j in range(3):
+            if (self.grid[i][j] == BOX):
+                openSpots += 1
+
+        if winner is None and openSpots == 0:
+            return "tie"
+        elif winner is not None:
+            winner = "X" if winner == CROSS_BOX else "O"
+            return winner
+        else:
+            return None
+        
 
 class Player:
     def __init__(self, name, symbol):
         self.score = 0
         self.name = name
         self.symbol = symbol
+        self.is_ai = False
+        self.symbol_art = ZERO_BOX if symbol == "0" else CROSS_BOX
 
     def add_score(self):
         self.score += 1
+
+
+
+
+class AIPlayer(Player):
+    def __init__(self, symbol):
+        super().__init__(name="Computer", symbol=symbol)
+        self.is_ai = True
+        self.scores = {
+              "X": 10,
+              "O": -10,
+              "tie": 0
+            };
+
+    def minimax_algorithm(self, board, depth, is_maximizing):
+        res = board.check_winner()
+        if res is not None:  # "X", "O", or "tie"
+            return self.scores[res]
+
+        if is_maximizing:
+            best_score = -float('inf')
+            for i in range(3):
+                for j in range(3):
+                    if board.grid[i][j] == BOX:
+                        board.grid[i][j] = self.symbol_art  # AI move
+                        score = self.minimax_algorithm(board, depth + 1, False)
+                        board.grid[i][j] = BOX
+                        best_score = max(score, best_score)
+            return best_score
+        else:
+            best_score = float('inf')
+            opponent_symbol = ZERO_BOX if self.symbol_art == CROSS_BOX else CROSS_BOX
+            for i in range(3):
+                for j in range(3):
+                    if board.grid[i][j] == BOX:
+                        board.grid[i][j] = opponent_symbol  # Opponent move
+                        score = self.minimax_algorithm(board, depth + 1, True)
+                        board.grid[i][j] = BOX
+                        best_score = min(score, best_score)
+            return best_score
+
+
+    
+    def best_move(self, board):
+        best_score = -float('inf')
+        pos = 0
+        best_position = 0
+        for i in range(0, 3):
+            for j in range(0, 3):
+                print("Position:", pos)
+                pos += 1
+                if board.grid[i][j] == BOX:
+                    board.grid[i][j] = self.symbol_art
+                    score = self.minimax_algorithm(board, 0, False)
+                    print(score)
+                    board.grid[i][j] = BOX
+                    if score > best_score:
+                        best_score = score
+                        best_position = pos 
+        print("Best Position: ##########################", best_position)
+
+
+        return best_position
--- Day84/main.py
+++ Day84/main.py
@@ -1,4 +1,4 @@
-from TicTacToe import TicTacToe, Player
+from TicTacToe import TicTacToe, Player, AIPlayer
 
 
 def run(player_1, player_2):
@@ -7,34 +7,67 @@ def run(player_1, player_2):
     turn = 0
     while True:
         current_player = player_1 if turn % 2 == 0 else player_2
-        positin = int(input(f"{current_player.name} - {current_player.symbol}, enter your position (1-9): "))
-        game.draw(positin, current_player.symbol)
+        if current_player.is_ai:
+            # AI logic to choose position
+            
+            position = current_player.best_move(game)
+            game.draw(position, current_player.symbol)
+        else:
+            position = int(input(f"{current_player.name} - {current_player.symbol}, enter your position (1-9): "))
+            game.draw(position, current_player.symbol)
 
-        win = game.check_winner(current_player)
-        if win:
+ 
+        win = game.check_winner()
+        if win == "tie":
+            print("It's a tie!")
             break
+        elif win in ["X", "O"]:
+            winner = player_1 if player_1.symbol == win else player_2
+            winner.add_score()
+            print(f"{winner.name} wins!")
+            print(f"Scores: {player_1.name} - {player_1.score}, {player_2.name} - {player_2.score}")
+            break
+        
 
         turn += 1
 
-def new_game():
-    player_1 = Player(name=input("Enter name for Player 1 (O): "), symbol='O')
-    player_2 = Player(name=input("Enter name for Player 2 (X): "), symbol='X')
+def new_game(ai=False):
+    if ai:
+        player_symbol = input("Choose your symbol [O/X]: ").upper()
+        if player_symbol == "O":
+            computer_symbol = "X"
+        else:
+            computer_symbol = "O"
 
-    run(player_1, player_2)
+        player_1 = Player(name=input(f"Enter name for Player 1 ({player_symbol}): "), symbol=player_symbol)
+        player_2 = AIPlayer(symbol=computer_symbol)
+    else:
+        player_1 = Player(name=input("Enter name for Player 1 (O): "), symbol='O')
+        player_2 = Player(name=input("Enter name for Player 2 (X): "), symbol='X')
+
+    run(player_1, player_2) 
     
     return [player_1, player_2]
 
 def start_game():
-    players = new_game()
+    opponent = input("Do you want to play against [computer - 1] or [friend - 2]? : ").lower()
+    if opponent == "1":
+        players = new_game(ai=True)
+    else:
+        players = new_game()
+
+
     while True:
         option_game = input("Do you want to [continue playing - 1], [start new game -2] or [exit - 3] ?: ").lower()
         if option_game  == "3":
             print("Thanks for playing!")
             break
         elif option_game == "2":
-            players = new_game()
+            players = start_game()
         elif option_game == "1":
             run(players[0], players[1])
+
+
 
 if __name__ == "__main__":
     start_game()
