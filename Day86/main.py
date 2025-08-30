bb6f0a0f1318c9b418ffe664b9d88b8eae557103
Marianna <mariannabenkalovych@gmail.com>
2025-08-31 01:01:37 +0300

    Add comments partially and update timer logic

--- Day86/main.py
+++ Day86/main.py
@@ -2,7 +2,8 @@ import tkinter as tk
 import kagglehub
 from tkinter import ttk
 import csv
-import time
+import math
+import sys
 
 # LINK
 # _______________________ How to get selected string __________________________
@@ -48,7 +49,57 @@ key_widths = {
 root = tk.Tk()
 root.title("Typing Speed Test")
 root.resizable(False, False)
-root.geometry("800x400")
+root.geometry("800x470")
+
+timer = None
+# ________________________ Navigation Bar ________________________________
+def exit():
+    sys.exit()
+
+def new_task(current_task_id=0):
+    refresh_task()
+    new_id = current_task_id+1
+    new_text = data_list[new_id][2]
+    if len(new_text) > 100:
+        for i in range(100, len(new_text), 100):
+            first_part = new_text[:i]
+            print(first_part)
+            second_part = new_text[i+1:] 
+            print(second_part)
+            new_text = first_part + "\n" + second_part
+            print(new_text)
+    task_text.config(text=new_text)
+    return new_id
+
+def refresh_task():
+    if timer is not None:
+        timer_label.after_cancel(timer)
+    timer_label.config(text="00:00")
+    entered_text.delete("1.0", tk.END)
+
+# Frame for Task and Timer
+navigation_button_bar = tk.Frame(root)
+navigation_button_bar.pack(fill='x')
+
+# Exit button
+exit_button = tk.Button(navigation_button_bar, text="Exit", width=10, borderwidth=2, bg="lightblue", command=exit)
+exit_button.pack(side="left", padx="5", pady='1')
+
+# New task button
+exit_button = tk.Button(navigation_button_bar, text="New Task", width=10, borderwidth=2, bg="lightblue", command=lambda id:new_task(id))
+exit_button.pack(side="left", padx="5", pady='1')
+
+# Retry button
+exit_button = tk.Button(navigation_button_bar, text=u"\u293B Refresh", width=10, borderwidth=2, bg="lightblue", command=refresh_task)
+exit_button.pack(side="left", padx="5", pady='1')
+
+# Info label
+info_label = tk.Label(navigation_button_bar, text="Info result: You are typing text...", borderwidth=2, fg='blue', bg='lightblue')
+info_label.pack(side="left", padx="5", pady='1', fill='x', expand=True)
+
+
+# ____________________________________ Task and Timer Bar _____________________________ 
+
 
 # Frame for Task and Timer
 task_clock_frame = tk.Frame(root)
@@ -62,6 +113,9 @@ task_text.pack(side='left',pady=5, fill='both', expand=True)
 timer_label = tk.Label(task_clock_frame, font=("Arial", 14), bg='lightblue', text="00:00")
 timer_label.pack(side="right", pady=5, fill='both')
 
+
+# __________________________ Entered Text Field ___________________________________
+# Entered text frame
 entered_text_frame = tk.Frame(root)
 entered_text_frame.pack()
 
@@ -75,6 +129,7 @@ scrollbar.pack(side="right", fill='y')
 
 entered_text.config(yscrollcommand=scrollbar.set)
 
+# ____________________________ Keyboard ____________________________
 # Keyboard frame
 keyboard_frame = tk.Frame(root)
 keyboard_frame.pack(padx=10, pady=10, fill=tk.Y)
@@ -199,6 +254,31 @@ def on_btn_press(t, mode="Pressed"):
     entered_text.insert(tk.END, char)
     entered_text.config(state=tk.DISABLED)
 
+def count_mistake(text1, text2):
+    text1_splited = text1.split('\n') # remove any \n becouse of my adding 
+    text1_plain = " ".join(text1_splited)
+    
+    text2_splited = text2.split('\n')
+    text2_plain = " ".join(text2_splited)
+
+    mistake_count = 0
+    for letter1, letter2 in zip(text1_plain, text2_plain):
+        if letter1 != letter2:
+            mistake_count += 1
+        
+    return mistake_count
+    
+def text_comprehansion():
+    entered_text = entered_text.get("1.0", tk.END)[:-1]
+    task_text = task_text.get("1.0", tk.END)[:-1]
+
+    if len(entered_text) == len(task_text):
+        info_label.config(text=f"You have mistakes: {count_mistake()}", fg="red")
+
+    if entered_text == task_text:
+        info_label.config(text="All right!", fg="green")
+
+    info_label.config(text="Info result: You are typing text...", fg="black")
 
 # Create tk keyboard
 buttons = []
@@ -269,29 +349,75 @@ for k in special_keys:
 root.bind("<KeyPress>", key_press)
 root.bind("<KeyRelease>", key_release)
 
-# ____________________ ADD LOGIC _________________________
+# ____________________ ADD TIMER LOGIC _________________________
 
-def clock(sec, count=0):
-    if count > sec:
+def start_timer(sec, format="starting"): 
+    """ 
+        Implement timer function. Update timer every second by one second less.
+        \nThere are two type: 
+        \nformat="starting" -  countdown for start (start from 0 to 3)
+        \nformat="timer" - countdown for game (start from 10:00)
+        
+    """
+    global timer
+    minutes = math.floor(sec / 60)
+    second = sec % 60
+
+    if format == "starting" and sec >= 0: # Check type
+        top_level_clock_process(sec) # Invoke clock func to procces top level window logic
+    elif format == "timer":
+        main_clock_process(sec, second, minutes)
+        text_comprehansion()
+
+
+def main_clock_process(sec, second,  minutes):
+    """ Formatting view of timer and actual implemnt countdown process for main timer in game"""
+
+    global timer # global timer variable for future "after" method cansellation.
+    
+    if second < 9: # make formatting for appropriate appearance
+        second = f'0{second}'
+
+    if minutes < 9: # make formatting for appropriate appearance
+        minutes = f'0{minutes}'
+
+    timer_label.config(text=f"{minutes}:{second}") # update timer text
+
+    if int(second) > 0 or int(minutes) > 0: # actual countdown process
+        timer = timer_label.after(100, start_timer, sec-1, "timer")
+
+
+def top_level_clock_process(sec):
+    """ Manage top level timer before starting game and implement countdown process """
+    clock_label.config(text=f"{sec}")
+    clock_label.after(1000, start_timer, sec-1) # put this statemnt for showing last update "start" during 1s after which the start_timer func won't start a new process with negative number
+
+    if sec == 0:
         clock_label.config(text="Start")
-        root.after(500, root.wm_attributes, '-topmost', True)
+
+        root.after(500, root.wm_attributes, '-topmost', True) # For hide the top level window smoothly by bringing up the main window
         root.after(500, entered_text.focus_set)
+
         clock_window.after(500, clock_window.destroy)
-    else:
-        clock_label.config(text=f"{count}")
-        clock_label.after(1000, clock, sec, count+1)
+        root.after(800, start_timer, 10*60, "timer")
+
+        current_task_id = new_task()
+        return current_task_id
+    
 
 
-clock_window = tk.Toplevel(root, bg="lightblue")  
-clock_window.title("Timer")
+clock_window = tk.Toplevel(root, bg="lightblue")  #Create top level window
+clock_window.title("Timer") 
 clock_window.geometry("300x200")
 
-clock_window.grab_set()
-clock_window.wm_attributes('-topmost', True)
+clock_window.grab_set() # top level windwos lestened to all event and "disable" listening to main window
+clock_window.wm_attributes('-topmost', True) # bring up the top level window
 
 clock_label = tk.Label(clock_window, width=250, height=100, font=("Aral", 36, "bold"), bg='lightblue')
 clock_label.pack(padx=50)
 
-clock(3)
+current_task_id = start_timer(3)
+
+
 
 root.mainloop()
